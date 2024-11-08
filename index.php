58136b9c9bd06beccd56a7fcf8bea575d9dcee34
jeewwee <jeewwee93@gmail.com>
2024-11-08 16:39:09 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores omnis magnam repellat reiciendis, distinctio facilis, sequi soluta rerum voluptatibus aliquam laudantium. Assumenda vero id ullam ab libero pariatur non aliquam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
