68c096fe9f7381cf78c8f17a1c941cbc2c290c7b
jeewwee <jeewwee93@gmail.com>
2024-11-08 16:32:17 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores omnis magnam repellat reiciendis, distinctio facilis, sequi soluta rerum voluptatibus aliquam laudantium. Assumenda vero id ullam ab libero pariatur non aliquam.</p>
 </body>
 </html>
